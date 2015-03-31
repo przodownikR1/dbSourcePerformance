9be5a708958761d9718c0703e5f0ea0c3e1cdfcc
Slawomir Borowiec <przodownik@tlen.pl>
2015-03-31 17:29:08 +0200

    dependencies changed

---
+++ src/main/java/demo/DbPerformanceApplication.java
@@ -0,0 +1,12 @@
+package demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DbPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbPerformanceApplication.class, args);
+    }
+}
